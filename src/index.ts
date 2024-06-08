7698f9933ed2d89e6a7d3d42d6ba5de11268d1fa
Paula Abib <paula.tws@hotmail.com>
2024-06-07 23:30:27 -0300

    feat: add mongodb client connection

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,33 @@
 import express from "express";
 import { config } from "dotenv";
+import { GetUsersController } from "./controllers/get-users/get-users";
+import { MongoGetUsersRepository } from "./repositories/mongo-get-users";
+import { MongoClient } from "./database/mongo";
 
-config();
-const app = express();
 
-const port = process.env.PORT || 8080;
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+const main = async () => {
 
-app.get("/", (req, res) => {
-    res.send("Hello World")
-})
+  config();
+  
+  const app = express();
+
+  await MongoClient.connect();
+
+  app.get("/users", async (req, res) => {
+    const mongoGetUsersRepository = new MongoGetUsersRepository();
+
+    const getUsersController = new GetUsersController(mongoGetUsersRepository);
+
+    const { body, statusCode } = await getUsersController.handle();
+
+    res.send(body).status(statusCode);
+  });
+
+  const port = process.env.PORT || 8080;
+
+  app.listen(port, () => console.log(`Listening on port ${port}`));
+};
+
+
+main();
