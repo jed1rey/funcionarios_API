a88cbee3f522196f536ecc8001910335db5d2a19
Paula Abib <paula.tws@hotmail.com>
2024-06-07 23:36:34 -0300

    feat: add ID to user

--- src/repositories/mongo-get-users.ts
+++ src/repositories/mongo-get-users.ts
@@ -1,15 +1,13 @@
 import { IGetUsersRepository } from "../controllers/get-users/protocols";
+import { MongoClient } from "../database/mongo";
 import { User } from "../models/user";
 
 export class MongoGetUsersRepository implements IGetUsersRepository {
   async getUsers(): Promise<User[]> {
+    const users = await MongoClient.db.collection<User>('users').find({}).toArray();
+
     return [
-      {
-        firstName: "Paula",
-        lastName: "Abib",
-        email: "paula.abib111@gmail.com",
-        password: "123",
-      },
+
     ];
   }
 }
