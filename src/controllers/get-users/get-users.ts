d5848862ad895fdbd0e76a93fb0b80d30c5d4e5b
Paula Abib <paula.tws@hotmail.com>
2024-06-07 21:48:45 -0300

    feat: add GetUsersController

---
+++ src/controllers/get-users/get-users.ts
@@ -0,0 +1,26 @@
+import { IGetUsersController, IGetUsersRepository } from "./protocols";
+
+export class GetUsersController implements IGetUsersController {
+  getUsersRepository: IGetUsersRepository;
+
+  constructor(getUsersRepository: IGetUsersRepository) {
+    this.getUsersRepository = getUsersRepository;
+  }
+
+  async handle() {
+    try {
+      //validar requisicoes
+      const users = await this.getUsersRepository.getUsers();
+
+      return {
+        statusCode: 200,
+        body: users,
+      };
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: "algo deu errado",
+      };
+    }
+  }
+}
---
+++ src/controllers/get-users/protocols.ts
@@ -0,0 +1,14 @@
+import {User} from '../../models/user'
+import { HttpResponse } from '../protocols'
+
+
+export interface IGetUsersController{
+    handle(): Promise<HttpResponse<User[]>>;
+}
+
+export interface IGetUsersRepository{
+
+    getUsers(): Promise<User[]>
+
+
+}
